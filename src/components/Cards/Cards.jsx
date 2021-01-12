313b750a4534eb29cf107751fed03868ba16fa6f
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-12 18:10:57 +0530

    demo cards displayed with matui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,58 @@
+import { Card, CardContent, Grid, Typography } from "@material-ui/core";
 import React from "react";
+import styles from "./Cards.module.css";
 
 function Cards(props) {
   console.log("From Cards");
   console.log(props.data);
-  return <div>Cards</div>;
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          {/* <Card> */}
+          <CardContent>
+            <Typography gutterBottom color="textSecondary">
+              Infected
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">
+              Number of active COVID19 cases
+            </Typography>
+          </CardContent>
+          {/* </Card> */}
+        </Grid>
+        <Grid item component={Card}>
+          {/* <Card> */}
+          <CardContent>
+            <Typography gutterBottom color="textSecondary">
+              Recovered
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID19
+            </Typography>
+          </CardContent>
+          {/* </Card> */}
+        </Grid>
+        <Grid item component={Card}>
+          {/* <Card> */}
+          <CardContent>
+            <Typography gutterBottom color="textSecondary">
+              Deaths
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="body2">
+              Number of deaths caused due to COVID19
+            </Typography>
+          </CardContent>
+          {/* </Card> */}
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Cards;
