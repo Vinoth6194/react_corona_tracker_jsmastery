cbff51cfd1f3a77fa1780083780bbe27af38d6b9
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-12 18:56:59 +0530

    multipe classes for styling

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import { Card, CardContent, Grid, Typography } from "@material-ui/core";
 import React from "react";
 import styles from "./Cards.module.css";
 import CounterUp from "react-countup";
+import cx from "classnames";
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   console.log("From Cards");
   //console.log(props.data);
@@ -15,7 +16,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
           {/* <Card> */}
           <CardContent>
             <Typography gutterBottom color="textSecondary">
@@ -38,7 +39,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
           {/* </Card> */}
         </Grid>
-        <Grid item component={Card} xs={12} md={3}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
           {/* <Card> */}
           <CardContent>
             <Typography gutterBottom color="textSecondary">
@@ -61,7 +62,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
           </CardContent>
           {/* </Card> */}
         </Grid>
-        <Grid item component={Card} xs={12} md={3}>
+        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
           {/* <Card> */}
           <CardContent>
             <Typography gutterBottom color="textSecondary">
