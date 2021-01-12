85eb3af9505dfccd12c14e19c5a8df1b3ef8f7ff
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-12 19:55:48 +0530

    fixed import in fetchDaily data in api

--- src/api/index.js
+++ src/api/index.js
@@ -39,11 +39,10 @@ export const fetchData = async () => {
     } = await axios.get(url);
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
-
-  const fetchDailyData = async () => {
-    try {
-      const { data } = await axios.get(`${url}/daily`);
-      console.log(data);
-    } catch (error) {}
-  };
+};
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    console.log(data);
+  } catch (error) {}
 };
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,18 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api";
+import {Bar, Line} from "react-chartjs-2";
 function Chart() {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchApi();
+  }, []);
+
+  
+     
   return <div>Chart</div>;
 }
 
