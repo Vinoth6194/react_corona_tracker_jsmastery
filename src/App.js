3be8d2424dd00f79fc6aa6760d23c91040002ab1
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-12 14:42:59 +0530

    components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Cards from "./components/Cards/Cards";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import Chart from "./components/Chart/Chart";
 
 function App() {
   return (
     <div className="App">
       <h1>Corona Tracker using Javascript Mystery</h1>
+      <Cards></Cards>
+      <CountryPicker></CountryPicker>
+      <Chart></Chart>
     </div>
   );
 }
