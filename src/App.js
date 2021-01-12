ad59ee1c57832de676a0ebfd66e9023115f5587e
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-12 15:05:27 +0530

    called the api in didmount

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 
 import React, { Component } from "react";
-
+import { fetchData } from "./api";
 export class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
   render() {
     return (
       <div className={styles.container}>
