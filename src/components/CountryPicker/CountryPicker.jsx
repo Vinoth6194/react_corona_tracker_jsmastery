8a5af122a4cfbcb8b6664f7bb5fc71cff6b51367
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-22 20:20:27 +0530

    countries maped to dropdown from state

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { countriesApi } from "../../api";
+import { NativeSelect } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import styles from "./Country.module.css";
 function CountryPicker() {
   const [fetchedCountries, setFetchedCountries] = useState([]);
   useEffect(() => {
@@ -12,7 +15,20 @@ function CountryPicker() {
   console.log(fetchedCountries.join("**"));
   console.log(fetchedCountries);
 
-  return <div>()</div>;
+  return (
+    <div className={styles.container}>
+      <FormControl>
+        <NativeSelect>
+          <option value="global">Global</option>
+          {fetchedCountries.map((country, i) => (
+            <option value={country} key={i}>
+              {country}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </div>
+  );
 }
 
 export default CountryPicker;
