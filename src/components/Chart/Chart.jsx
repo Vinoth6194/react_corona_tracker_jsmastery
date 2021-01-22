e4744e6218ca90223e8d6e5fec6735838bf4ba26
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-22 17:33:04 +0530

    dailydata api is implelented and console logged

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,17 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { fetchDailyData } from "../../api";
+import { Bar, Line } from "react-chartjs-2";
 function Chart() {
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchDailyApi = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    fetchDailyApi();
+    console.log("Dailydata ----" + dailyData);
+  }, []);
+
   return <div>Chart</div>;
 }
 
