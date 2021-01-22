825be2833324137dafc24270d8bba9a0b6592e0a
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-01-22 16:27:33 +0530

    demo card for infected cases

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styles from "./Cards.module.css";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 function Cards(props) {
   console.log("From Cards");
   console.log(props.data);
-  return <div>Cards</div>;
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item>
+          <Card>
+            <CardContent>
+              <Typography color="textSecondary" guttterBottom>
+                Infected
+              </Typography>
+              <Typography variant="h5">Real Data</Typography>
+              <Typography color="textSecondary">Real Date</Typography>
+              <Typography variant="body2">
+                Number of active case of COVID-19
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Cards;
